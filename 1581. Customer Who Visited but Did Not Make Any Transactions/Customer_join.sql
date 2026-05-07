2ff5bfc87237355067ae991bf32881a6420c5ece
Adarsh-K17 <88191622+Adarsh-K17@users.noreply.github.com>
2026-05-07 21:07:16 +0000

    Day-2 of 50 SQL leetcode challange

---
+++ 1581. Customer Who Visited but Did Not Make Any Transactions/Customer_join.sql
@@ -0,0 +1,7 @@
+select customer_id, count(*) as count_no_trans
+from visits left join transactions on 
+visits.visit_id = transactions.visit_id 
+where transactions.transaction_id is null 
+group by customer_id
+
+--keep the table on left whose all columns are needed
